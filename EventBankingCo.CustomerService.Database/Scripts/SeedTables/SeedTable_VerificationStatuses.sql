1a595a9df3f7d89065694cb6a1dc809e18807da6
Daniel Aguirre <uhyeay24@gmail.com>
2025-06-14 22:16:11 -0500

    refactor seed table scripts to remove variable name and transaction

--- EventBankingCo.CustomerService.Database/Scripts/SeedTables/SeedTable_VerificationStatuses.sql
+++ EventBankingCo.CustomerService.Database/Scripts/SeedTables/SeedTable_VerificationStatuses.sql
@@ -1,31 +1,27 @@
-﻿BEGIN TRAN
-    DECLARE @TableName NVARCHAR(128) = '[dbo].[VerificationStatuses]';
+﻿PRINT 'Seeding [dbo].[VerificationStatuses] table.';
 
-    PRINT 'Seeding Table ' + @TableName;
+MERGE [dbo].[VerificationStatuses] AS target
+    USING (
+        VALUES 
+            (1, 'PendingVerification', 'Customer is awaiting identity or email verification'),
+            (2, 'Verified', 'Customer identity has been successfully verified'),
+            (3, 'Rejected', 'Customer verification failed or was denied'),
+            (4, 'Suspended', 'Customer is temporarily suspended due to review or issues')
+    ) AS source (Id, Name, Description)
+    ON target.Id = source.Id
+    WHEN MATCHED AND (
+        target.Name <> source.Name OR 
+        target.Description <> source.Description
+    ) THEN
+        UPDATE SET 
+            Name = source.Name, 
+            Description = source.Description
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (Id, Name, Description)
+        VALUES (source.Id, source.Name, source.Description)
+    WHEN NOT MATCHED BY SOURCE THEN
+        DELETE
+;
 
-    MERGE dbo.VerificationStatuses AS target
-        USING (
-            VALUES 
-                (1, 'PendingVerification', 'Customer is awaiting identity or email verification'),
-                (2, 'Verified', 'Customer identity has been successfully verified'),
-                (3, 'Rejected', 'Customer verification failed or was denied'),
-                (4, 'Suspended', 'Customer is temporarily suspended due to review or issues')
-        ) AS source (Id, Name, Description)
-        ON target.Id = source.Id
-        WHEN MATCHED AND (
-            target.Name <> source.Name OR 
-            target.Description <> source.Description
-        ) THEN
-            UPDATE SET 
-                Name = source.Name, 
-                Description = source.Description
-        WHEN NOT MATCHED BY TARGET THEN
-            INSERT (Id, Name, Description)
-            VALUES (source.Id, source.Name, source.Description)
-        WHEN NOT MATCHED BY SOURCE THEN
-            DELETE
-    ;
-
-    DECLARE @RowCount INT = @@ROWCOUNT;
-    PRINT @TableName + 'has been seeded. Rows affected: ' + CAST(@RowCount AS VARCHAR(10));
-COMMIT TRAN
+DECLARE @RowCount INT = @@ROWCOUNT;
+PRINT 'Completed seeding [dbo].[VerificationStatuses] table. Rows affected: ' + CAST(@RowCount AS VARCHAR(10));
